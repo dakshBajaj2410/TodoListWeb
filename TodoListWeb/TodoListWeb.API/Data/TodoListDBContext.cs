898f26683215838ad5aad41dbf46183cfd6dfa9e
dakshbajaj2410 <dakshbajaj2410@gmail.com>
2022-08-16 23:15:36 +0530

    Created Domain

---
+++ TodoListWeb/TodoListWeb.API/Data/TodoListDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListWeb.API.Models.Domain;
+
+namespace TodoListWeb.API.Data
+{
+    public class TodoListDBContext: DbContext
+    {
+        public TodoListDBContext(DbContextOptions<TodoListDBContext> options): base(options)
+        {
+           
+        }
+
+        public DbSet<TodoItems> TodoItems { get; set; }
+    }
+}
---
+++ TodoListWeb/TodoListWeb.API/Models/Domain/TodoItems.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoListWeb.API.Models.Domain
+{
+    public class TodoItems
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        //Navigation Properties
+        //NONE
+    }
+}
